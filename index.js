d0bd8c2e238ac38b32d7675c271fea4fe8a8b6d7
mohammadshahed <silentshahed@gmail.com>
2020-10-11 03:45:01 +0600

    node fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 
 // mongodb
-const ObjectId = require('mongodb').ObjectID;
+const ObjectId = require('mongodb').ObjectId;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sgixs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 // mongodb client
